0088adf2d4329f4c3705d7d1f6596a5460bf58a5
jak1841 <jaskarnd16@gmail.com>
2022-10-23 13:07:51 -0500

    created matchtoken_val, matchtoken_type, match_symbol, is_next_token_type_same and restructured each rule function with those functions in it to make code easier to read

--- src/syntax_analysis.py
+++ src/syntax_analysis.py
@@ -12,7 +12,8 @@
 
     print_statement: print(string); | print(exp) | print(identifier);
 
-    assignment -> identifier = [String | exp | identifier | boolean];
+    assignment -> datatype identifier = [String | exp | identifier | boolean];
+
 
 
     bool_exp -> bool_exp or bool_term | bool_term
@@ -40,11 +41,50 @@ class syn_analysis:
         self.sym_table = {}
         self.tokens = tokens
 
-    # this method used when expected symbol not correct symbol
-    def expected_token(self, expected_token):
+    # Given a token_type will return true if top of token list has same token_type
+    # if not return false
+    def is_next_token_type_same(self, token_type):
+        if (len(self.tokens) > 0 and self.tokens[0][0] == token_type):
+            return True
+        return False
+
+    # Given a token_type will check that the top of the token list has that specefic
+    # tokentype and if it does pop return the token, if not => error
+    def match_token_type(self, token_type):
+        if (len(self.tokens) > 0 and self.tokens[0][0] == token_type):
+            return self.tokens.pop(0)
+
+        # error if not same or tokens list is empty
         if (len(self.tokens) > 0):
-            raise Exception("Expected", expected_token, "but got", self.tokens[0])
-        raise Exception("Expected", expected_token, "but got", None)
+            raise Exception("Expected", token_type, "but got", self.tokens[0])
+        raise Exception("Expected", token_type, "but got", None)
+
+    # Given a token value will check that the top of the token list has that specefic
+    # token val and if it does pop return the token, if not => erro
+    def match_token_val(self, token_val):
+        if (len(self.tokens) > 0 and self.tokens[0][1] == token_val):
+            return self.tokens.pop(0)
+
+        # error if not same or tokens list is empty
+        if (len(self.tokens) > 0):
+            raise Exception("Expected", token_val, "but got", self.tokens[0])
+        raise Exception("Expected", token_val, "but got", None)
+
+    # Given a symbol_token checks if it is in the symbol table, if is => return symbol_token
+    # if not => erro
+    def match_symbol(self, symbol_token):
+        if (symbol_token in self.sym_table):
+            return symbol_token
+        raise Exception("Unexpected symbol", symbol_token)
+
+
+
+
+    # where the error is in the code
+    def expected_token(self, expected_type):
+        if (len(self.tokens) > 0):
+            raise Exception("Expected", expected_type, "but got", self.tokens[0])
+        raise Exception("Expected", expected_type, "but got", None)
 
     # error when encountering unexpected symbol
     def unexpected_symbol(self):
@@ -131,23 +171,17 @@ class syn_analysis:
 
     # assuming that list of tokens lead to a print identifier, executes that line
     def print_identifier(self):
-        self.tokens.pop(0) # consume print
-        self.tokens.pop(0) # consumes (
+        self.match_token_type("Print")
+        self.match_token_val("(")
+        identifier_token = self.match_token_type("identifier")
 
-        cur = self.tokens.pop(0) # consumes identfier
-
-        if (cur not in self.sym_table):
-            raise Exception("unexpected symbol:", cur)
-
-        value = self.sym_table[cur] # retrives the value at the symbol table
+        symbol = self.match_symbol(identifier_token)
+        value = self.sym_table[symbol] # retrives the value at the symbol table
         print(value)
 
-        if (len(self.tokens) > 1):
-            if (self.tokens[0][1] == ")" and self.tokens[1][1] == ";"):
-                self.tokens.pop(0) # consume right paranthesis
-                self.tokens.pop(0) # consume the end semicolon
-                return
-        self.expected_token(") or ;")
+        self.match_token_val(")")
+        self.match_token_val(";")
+
 
 
 
@@ -176,34 +210,21 @@ class syn_analysis:
 
     # assuming that the list of tokens lead to a print an math expression executes that line
     def print_math_expression(self):
-        if (len(self.tokens) > 0 and self.tokens[0][0] == "Print"):
-            self.tokens.pop(0)
-            if (len(self.tokens) > 0 and self.tokens[0][1] == "("):
-                self.tokens.pop(0)
-                x = self.exp()
-                if (len(self.tokens) > 0 and self.tokens[0][1] == ")"):
-                    self.tokens.pop(0)
-                    if (len(self.tokens) > 0 and self.tokens[0][1] == ";"):
-                        self.tokens.pop(0)
-                        print(x)
-                        return
-        raise Exception("Syntax error", self.tokens)
+        self.match_token_type("Print")
+        self.match_token_val("(")
+        print(self.exp())
+        self.match_token_val(")")
+        self.match_token_val(";")
+
 
     # assuming that the list of tokens leads to a print strign exceutes that line
     def print_string(self):
-        self.tokens.pop(0) # consumes print statement
-        self.tokens.pop(0) # consume left paranthesis
-        print(self.tokens.pop(0)[1]) # print the string
-
-        if (len(self.tokens) > 0 and self.tokens[0][1] == ")"):
-            self.tokens.pop(0) # consumes right paranthesis
-            if (len(self.tokens) > 0 and self.tokens[0][1] == ";"):
-                self.tokens.pop(0)
-            else:
-                raise Exception("Expected a ; but got ", self.tokens)
-
-        else:
-            raise Exception("Expected a ) but got ", self.tokens)
+        self.match_token_type("Print") # consumes print statement
+        self.match_token_val("(") # consume left paranthesis
+        string_token = self.match_token_type("string")
+        print(string_token[1]) # print the string
+        self.match_token_val(")")
+        self.match_token_val(";")
 
     # given a list of tokens will return true if it prints an string
     def _is_print_string(self):
@@ -216,25 +237,21 @@ class syn_analysis:
 
     # assuming that the list of tokens leads to identfier assignment executes that line
     def identfier_assignment(self):
-        ident = self.tokens.pop(0) # consumes identifier
-        self.tokens.pop(0) # consume equality
+        ident = self.match_token_type("identifier")
+        self.match_token_val("=")
 
-        if (len(self.tokens) > 0 and self.tokens[0][0] == "string"):
-            self.sym_table[ident] = self.tokens.pop(0)[1] # add new value to symbol table
-        elif (len(self.tokens) > 0 and self.tokens[0][0] == "num"):
+        if (self.is_next_token_type_same("string")):
+            self.sym_table[ident] = self.match_token_type("string")[1]
+        elif (self.is_next_token_type_same("num")):
             self.sym_table[ident] = self.exp()
-        elif (len(self.tokens) > 0 and self.tokens[0][1] == "("):
+        elif (self.is_next_token_type_same("paranthesis")):
             self.sym_table[ident] = self.exp()
-        elif (len(self.tokens) > 0 and self.tokens[0][0] == "identifier"):
+        elif (self.is_next_token_type_same("identifier")):
             self.sym_table[ident] = self.exp()
         else:
             raise Exception("expected a num or string but got", self.tokens)
 
-        # ensures ; at the end
-        if (len(self.tokens) > 0 and self.tokens[0][1] == ";"):
-            self.tokens.pop(0)
-            return
-        raise Exception("expected ; but got", self.tokens)
+        self.match_token_val(";")
 
     # given a list of tokens will reture true if it is an identfier assigment statement
     def _is_identifier_assigment(self):
